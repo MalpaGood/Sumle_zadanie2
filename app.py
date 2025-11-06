e5cce2dc4be3772837a30d044375359133eab471
Aleksander Radoliński <121904755+MalpaGood@users.noreply.github.com>
2025-11-06 22:11:27 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pickle
+import numpy as np
+from datetime import datetime
+
+starttime = datetime.now()
+
+filename = "model.h5"
+model = pickle.load(open(filename, 'rb'))
+
+sex_d = {0: "Kobieta", 1: "Mężczyzna"}
+pclass_d = {0: "Pierwsza", 1: "Druga", 2: "Trzecia"}
+embarked_d = {0: "Cherbourg", 1: "Queenstown", 2: "Southampton"}
+
+def main():
+    st.set_page_config(page_title="Czy przeżyłbyś katastrofę?")
+    
+    st.image("https://media1.popsugar-assets.com/files/thumbor/7CwCuGAKxTrQ4wPyOBpKjSsd1JI/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/04/19/743/n/41542884/5429b59c8e78fbc4_MCDTITA_FE014_H_1_.JPG")
+    st.title("Czy przeżyłbyś katastrofę Titanica?")
+
+    left, right = st.columns(2)
+
+    with left:
+        sex_radio = st.radio("Płeć", list(sex_d.keys()), format_func=lambda x: sex_d[x])
+        pclass_radio = st.radio("Klasa", list(pclass_d.keys()), format_func=lambda x: pclass_d[x])
+        embarked_radio = st.radio("Port zaokrętowania", list(embarked_d.keys()), format_func=lambda x: embarked_d[x])
+
+    with right:
+        age_slider = st.slider("Wiek", value=30, min_value=1, max_value=100)
+        sibsp_slider = st.slider("Liczba rodzeństwa/partnerów na pokładzie", min_value=0, max_value=8)
+        parch_slider = st.slider("Liczba rodziców/dzieci na pokładzie", min_value=0, max_value=6)
+        fare_slider = st.slider("Cena biletu", min_value=0, max_value=500, step=5)
+
+    data = np.array([[pclass_radio, sex_radio, age_slider, sibsp_slider, parch_slider, fare_slider]])
+
+    survival = model.predict(data)
+    s_confidence = model.predict_proba(data)
+
+    st.markdown("---")
+    st.header(f"Czy dana osoba przeżyje? **{'Tak' if survival[0] == 1 else 'Nie'}**")
+    st.subheader(f"Pewność predykcji: **{s_confidence[0][survival[0]] * 100:.2f}%**")
+
+if __name__ == '__main__':
+    main()
